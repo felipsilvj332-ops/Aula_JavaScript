de9b5e14b09518679bebeee2473052570195f7cf
Felipe.Tec. <felipsilvj332@gmail.com>
2026-07-08 00:04:59 -0300

    Aula05
    JS não esta comunicando com o site

--- Aula01_ player_de_música/script.js
+++ Aula01_ player_de_música/script.js
@@ -1,3 +1,5 @@
-const songName = document.getElementById("song-name");
+const songName = document.getElementById('song-name');
+const song = document.getElementById('audio');
 
 songName.innerText = "não definido";
+song.play();
